b337a490d88cc3c33409b9051044173a212e5734
John Anderson <jfanderson373@gmail.com>
2015-07-28 21:59:35 -0500

    Added checks for empty arrays/objects. Additional debugging

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,22 +4,33 @@
 // but you don't so you're going to write it from scratch:
 
 var stringy = function(obj) {
-  if (Array.isArray(obj)) {
-    var str += '[';
+  if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'string') {
+    return '"' + String(obj) + '"';
+  } else if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    var str = '[';
     obj.forEach(function(item) {
       str += stringy(item) + ',';
     });
     str = str.slice(0,-1) + ']';
     return str;
   } else if (typeof obj === 'object') {
-    var str += '{';
+    var str = '{';
     for (var key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        continue;
+      }
       str += stringy(key) + ':' + stringy(obj[key]) + ',';
     }
-    str = str.slice(0,-1) + '}';
-    return str;
-  } else if (typeof obj === 'string') {
-    return '"' + String(obj) + '"';
+    if (str[str.length-1] === ',') {
+      return str.slice(0,-1) + '}';
+    } else {
+      return str + '}';
+    }
   } else {
     return String(obj);
   }
