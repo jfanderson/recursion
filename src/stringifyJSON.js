6a0fd45af96958e7362dc6fe1f535b75634ad0e7
John Anderson <jfanderson373@gmail.com>
2015-07-28 19:54:07 -0500

    stringifyJSON.js can handle arrays and primitives

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,22 @@
 
 // but you don't so you're going to write it from scratch:
 
+var stringy = function(obj) {
+  var str = '';
+  if (Array.isArray(obj)) {
+    str += '[';
+    obj.forEach(function(item) {
+      str += stringy(item) + ',';
+    });
+    str = str.slice(0,-1) + ']';
+  } else if (typeof obj === 'string') {
+    str += '"' + String(obj) + '"';
+  } else {
+    str += String(obj);
+  }
+  return str;
+}
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  return '' + stringy(obj);
 };
